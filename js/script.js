f31a4d8b9336a388ddd3b491468d57f73cb98972
patelhit125 <59541507+patelhit125@users.noreply.github.com>
2020-11-13 10:58:47 +0530

    upload website
    First master

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("navbarToggle").addEventListener("click", function () {
+    document.getElementById("navbarToggler").classList.toggle("open");
+    document.getElementById("navbarContainer").classList.toggle("open");
+});
