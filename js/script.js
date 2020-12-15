97a7bd5d3b8255406617552ee3ddb05d96a3692f
patelhit125 <59541507+patelhit125@users.noreply.github.com>
2020-12-15 17:35:59 +0530

    ADDED: Covid-19 cases numbers

--- js/script.js
+++ js/script.js
@@ -52,9 +52,32 @@ $(document).ready(function () {
   $.ajax(settings).done(function (json) {
     var html = '<div class="row gutterRow">';
     $.each(json.value, function(index) {
-        html += '<div class="coloumn-3 gutter"><img src="' + json.value[index].image.thumbnail.contentUrl + '&w=412&h=264&c=14&rs=2&qlt=100&dpr=1.5"  alt="' + json.value[index].name + '" class="imageFluid"><div>' + json.value[index].name + '</div></div>';
+        html += '<div class="coloumn-3 gutter"><a class="newsLink" href="' + json.value[index].url + '" target="_blank" rel="noopener noreferrer"><img src="' + json.value[index].image.thumbnail.contentUrl + '&w=412&h=264&c=14&rs=2&qlt=100&dpr=1.5"  alt="' + json.value[index].name + '" class="imageFluid newsImage"><div>' + json.value[index].name + '</a></div></div>';
     });
     html += '</div>';
     $('#news').append(html);
   });
+
+  $.ajax("https://api.covid19india.org/data.json").done(function (data) {
+    var dailyactive_ = data.statewise[0].deltaconfirmed - data.statewise[0].deltarecovered - data.statewise[0].deltadeaths;
+    if (dailyactive_ > 0) {
+      dailyactive = dailyactive_;
+    } else {
+      dailyactive_ = 0;
+    }
+    $("#totalConfirmed").html(Number(data.statewise[0].confirmed).toLocaleString('en-IN'));
+    $("#totalActive").html(Number(data.statewise[0].active).toLocaleString('en-IN'));
+    $("#totalRecovered").html(Number(data.statewise[0].recovered).toLocaleString('en-IN'));
+    $("#totalDeceased").html(Number(data.statewise[0].deaths).toLocaleString('en-IN'));
+    $("#dailyRecovered").html(Number(data.statewise[0].deltarecovered).toLocaleString('en-IN'));
+    $("#dailyConfirmed").html(Number(data.statewise[0].deltaconfirmed).toLocaleString('en-IN'));
+    $("#dailyDeceased").html(Number(data.statewise[0].deltadeaths).toLocaleString('en-IN'));
+    $("#dailyActive").html(Number(dailyactive).toLocaleString('en-IN'));
+    $("#lastUpdated").html("Last update: " + data.statewise[0].lastupdatedtime);
+  });
 });
+
+(function($) {
+  'use strict';
+  $('.count-num').rCounter();
+})(jQuery);
